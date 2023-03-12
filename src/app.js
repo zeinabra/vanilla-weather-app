d667f76995bbba80aa1b38c1dee93015d953da23
zeinabra <zeinab.rasouli@gmail.com>
2023-03-12 16:53:01 +0330

    showing tempreture

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+function displayTempreture(response) {
+  let tempretureElement = document.querySelector("#temperature");
+  tempretureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "85bbd3d16a2dfe0ecf253c7ae1e8fe03";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTempreture);
