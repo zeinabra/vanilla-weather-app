c0fe6e67109b5927b232171892fa5a549e1fa0ec
zeinabra <zeinab.rasouli@gmail.com>
2023-03-15 17:41:45 +0330

    fix apiUrl error

--- src/app.js
+++ src/app.js
@@ -27,11 +27,11 @@ function formatDay(timestamp) {
   return days[day];
 }
 function displayForecast(response) {
-  let foreCast = response.data.daily;
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  foreCast.forEach(function (forecastDay, index) {
-    if (index < 5) {
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
       forecastHTML =
         forecastHTML +
         `
@@ -64,8 +64,8 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates) {
-  apiKey = "85bbd3d16a2dfe0ecf253c7ae1e8fe03";
-  apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}$units=metric`;
+  apiKey = "842b36d55cb28eba74a018029d56b04c";
+  apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
